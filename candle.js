f4b9a70aebe97c0c5115344dc8f4b9ae75428a39
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2012-11-01 03:58:43 +0700

    added debug; using a faster timers.js

--- candle.js
+++ candle.js
@@ -1,3 +1,7 @@
+var timers = require('./timers.js');
+//var timers = require('timers');
+
+var debug = require('debug')('candle');
 var candle = function() {
     this.callbacks = {};
     this.id = 0;
@@ -8,22 +12,25 @@ candle.prototype.get_timeout = function(id) {
 };
 candle.prototype.add = function(callback, timeout) {
     var id = ++this.id;
-    var timer = setTimeout(this.get_timeout(id), timeout);
+    debug('Added callback', 'id:', id, ', timeout:', timeout);
+    var timer = timers.setTimeout(this.get_timeout(id), timeout);
     this.callbacks[id] = [callback, timer];
     return id;
 };
 candle.prototype.timeout = function(id) {
+    debug('Callback timed out', 'id:', id);
     this.resolve(id, 'timeout');
 };
-candle.prototype.resolve = function(id, err) {
+candle.prototype.resolve = function(id, err, result) {
+    debug('Callback resolved', 'id:', id);
     var l = arguments.length;
     var s = this.callbacks[id];
     if (s) {
         this.delete(id);
         var c = s[0];
         if (l == 1) c();
-        else if (l == 2) c(arguments[1]);
-        else if (l == 3) c(arguments[1], arguments[2]);
+        else if (l == 2) c(err);
+        else if (l == 3) c(err, result);
         else {
             var args = new Array(l - 1);
             for (var i = 1; i < l; i++) args[i - 1] = arguments[i];
@@ -35,7 +42,7 @@ candle.prototype.resolve = function(id, err) {
 };
 candle.prototype.delete = function(id) {
     if (this.callbacks[id] && this.callbacks[id][1]) {
-        clearTimeout(this.callbacks[id][1]);
+        timers.clearTimeout(this.callbacks[id][1]);
     }
     delete this.callbacks[id];
 };
