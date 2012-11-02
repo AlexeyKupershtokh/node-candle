60a9a13fc6ece0e2d20c5a8927aa3d8bb438edad
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2012-11-02 22:36:39 +0700

    added yet another example

---
+++ examples/simle-timeout.js
@@ -0,0 +1,11 @@
+var candle = require('..').candle;
+
+// Create a new candle, usually you will need only one since it can handle many callbacks.
+var c = new candle();
+
+// Add a callback to it
+var id = c.add(function(err, response) { console.log('callback fired,', err, response); })
+
+c.setTimeout(id, 1000);
+
+// output after 1 second: callback fired, timeout undefined
