6436b1a037f855c78490e720900b319f35f0bddb
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2012-11-02 22:50:39 +0700

    optimizations

--- candle.js
+++ candle.js
@@ -4,29 +4,28 @@ var timers = require('./timers.js');
 var debug = require('debug')('candle');
 var candle = function() {
     this.callbacks = Object.create(null);
-    this.timeouts = Object.create(null);
     this.id = 0;
     this.timeoutResolver = null;
 };
 candle.prototype.add = function(callback) {
     var id = ++this.id;
     debug('add(...), assigned id = ' + id);
-    this.callbacks[id] = callback;
+    this.callbacks[id] = [callback, null];
     return id;
 };
 candle.prototype.resolve = function(id, err, result) {
     debug('resolve(' + id + ', ...)');
     var l = arguments.length;
     var callback = this.callbacks[id];
-    if (callback) {
+    if (callback && callback[0]) {
         this.delete(id);
-        if (l == 1) callback();
-        else if (l == 2) callback(err);
-        else if (l == 3) callback(err, result);
+        if (l == 1) callback[0]();
+        else if (l == 2) callback[0](err);
+        else if (l == 3) callback[0](err, result);
         else {
             var args = new Array(l - 1);
             for (var i = 1; i < l; i++) args[i - 1] = arguments[i];
-            callback.apply(this, args);
+            callback[0].apply(this, args);
         }
     }
 };
@@ -39,14 +38,14 @@ candle.prototype.setTimeout = function(id, timeout) {
     debug('setTimeout(' + id + ')');
     if (!this.callbacks[id]) return;
     this.clearTimeout(id);
-    this.timeouts[id] = timers.setTimeout(this.getTimeout(id), timeout);
+    this.callbacks[id][1] = timers.setTimeout(this.getTimeout(id), timeout);
 };
 candle.prototype.clearTimeout = function(id) {
     debug('clearTimeout(' + id + ')');
-    if (this.timeouts[id]) {
-      timers.clearTimeout(this.timeouts[id]);
+    if (this.callbacks[id] && this.callbacks[id][1]) {
+      timers.clearTimeout(this.callbacks[id][1]);
     }
-    delete this.timeouts[id];
+    this.callbacks[id][1] = null;
 };
 candle.prototype.getTimeout = function(id) {
     var self = this;
