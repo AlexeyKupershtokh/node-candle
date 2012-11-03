528f342f60a80eaf63669f9eecd5daf6dbe5798e
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2012-11-04 05:36:29 +0700

    jslint-driven improvements

--- candle.js
+++ candle.js
@@ -1,65 +1,77 @@
+/*jslint indent: 4 */
 var timers = require('./timers.js');
 //var timers = require('timers');
 
 var debug = require('debug')('candle');
-var candle = function() {
+var candle = function () {
+    "use strict";
     this.callbacks = Object.create(null);
     this.id = 0;
     this.timeoutResolver = null;
 };
-candle.prototype.add = function(callback) {
+candle.prototype.add = function (callback) {
+    "use strict";
     var id = ++this.id;
     debug('add(...), assigned id = ' + id);
     this.callbacks[id] = [callback, null];
     return id;
 };
-candle.prototype.resolve = function(id, err, result) {
+candle.prototype.resolve = function (id, err, result) {
+    "use strict";
     debug('resolve(' + id + ', ...)');
-    var l = arguments.length;
-    var callback = this.callbacks[id];
+    var l = arguments.length, callback = this.callbacks[id], i, args;
     if (callback && callback[0]) {
-        this.delete(id);
-        if (l == 1) callback[0]();
-        else if (l == 2) callback[0](err);
-        else if (l == 3) callback[0](err, result);
-        else {
-            var args = new Array(l - 1);
-            for (var i = 1; i < l; i++) args[i - 1] = arguments[i];
+        this.remove(id);
+        if (l === 1) {
+            callback[0]();
+        } else if (l === 2) {
+            callback[0](err);
+        } else if (l === 3) {
+            callback[0](err, result);
+        } else {
+            args = new Array(l - 1);
+            for (i = 1; i < l; i++) { args[i - 1] = arguments[i]; }
             callback[0].apply(this, args);
         }
     }
 };
-candle.prototype.delete = function(id) {
-    debug('delete(' + id + ')');
+candle.prototype.remove = function (id) {
+    "use strict";
+    debug('remove(' + id + ')');
     this.clearTimeout(id);
     delete this.callbacks[id];
 };
-candle.prototype.setTimeout = function(id, timeout) {
+candle.prototype.setTimeout = function (id, timeout) {
+    "use strict";
     debug('setTimeout(' + id + ')');
-    if (!this.callbacks[id]) return;
+    if (!this.callbacks[id]) { return; }
     this.clearTimeout(id);
     this.callbacks[id][1] = timers.setTimeout(this.getTimeout(id), timeout);
 };
-candle.prototype.clearTimeout = function(id) {
+candle.prototype.clearTimeout = function (id) {
+    "use strict";
     debug('clearTimeout(' + id + ')');
     if (this.callbacks[id] && this.callbacks[id][1]) {
-      timers.clearTimeout(this.callbacks[id][1]);
+        timers.clearTimeout(this.callbacks[id][1]);
     }
     this.callbacks[id][1] = null;
 };
-candle.prototype.getTimeout = function(id) {
+candle.prototype.getTimeout = function (id) {
+    "use strict";
     var self = this;
-    return function() { return self.onTimeout(id); };
+    return function () { return self.onTimeout(id); };
 };
-candle.prototype.onTimeout = function(id) {
+candle.prototype.onTimeout = function (id) {
+    "use strict";
     debug('onTimeout(' + id + ')');
-    if (typeof this.timeoutResolver == 'function') {
-      this.timeoutResolver(id);
+    if (typeof this.timeoutResolver === 'function') {
+        this.timeoutResolver(id);
     } else {
-      this.resolve(id, 'timeout');
+        this.resolve(id, 'timeout');
     }
 };
-candle.prototype.setTimeoutResolver = function(callback) {
+candle.prototype.setTimeoutResolver = function (callback) {
+    "use strict";
     this.timeoutResolver = callback;
 };
 module.exports.candle = candle;
